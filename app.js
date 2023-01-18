430f56d2a8780e3835b0137034863407a24085d6
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-01-18 16:05:10 +0100

    Modification

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+// Import d'express
 const express = require('express');
+
+// Utilisation d'express
 const app = express();
+
+// Accès au core de la requête
+app.use(express.json());
+
+// Import de MongoDB
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
-const stuffRoutes = require('./routes/stuff');
+
+const Sauce = require('./models/sauce');
+
+const path = require('path');
+
+// Import des routes
+const sauceRoutes = require('./routes/sauce');
+
+// Import des routes
 const userRoutes = require('./routes/user');
 
+const cors = require('cors');
+
+// Configuration de la base de données mongoDB
 mongoose.connect('mongodb+srv://admin:admin@clusterpiiquante.50ks1cn.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -12,27 +30,38 @@ mongoose.connect('mongodb+srv://admin:admin@clusterpiiquante.50ks1cn.mongodb.net
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
   
+  app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use(express.json());
+// Ajout des Middlewares d'autorisations
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();  
+});
 
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
+  app.get('/api/sauce/:id', (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(404).json({ error }));
+  });
+
+
+  app.get('/api/sauces', (req, res, next) => {
+    Sauce.find()
+      .then(sauce => res.status(200).json(sauce))
       .catch(error => res.status(400).json({ error }));
   });
-  
 
-  app.use('/api/sauces', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-app.use('/api/sauces', stuffRoutes);
+app.use(cors());
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res, next) => { 
+exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
         const user = new User({
@@ -18,26 +18,26 @@ exports.signup = (req, res, next) => {
   
 
 exports.login = (req, res, next) => {
-   User.findOne({ email: req.body.email })
-       .then(user => {
-           if (!user) {
-               return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-           }
-           bcrypt.compare(req.body.password, user.password)
-               .then(valid => {
-                   if (!valid) {
-                       return res.status(401).json({ error: 'Mot de passe incorrect !' });
-                   }
-                   res.status(200).json({
-                       userId: user._id,
-                       token: jwt.sign(
-                           { userId: user._id },
-                           'RANDOM_TOKEN_SECRET',
-                           { expiresIn: '24h' }
-                       )
-                   });
-               })
-               .catch(error => res.status(500).json({ error }));
-       })
-       .catch(error => res.status(500).json({ error }));
-};
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+ };
