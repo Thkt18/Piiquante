d0ef83f343f4b007db5053f868cf4738a68a57d1
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-02-03 12:47:05 +0100

    Ajout de "password-validator"

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express'); // import express
 const router = express.Router(); // create a router
 const max = require('../middleware/limit'); // import the limit middleware
+const password = require('../middleware/password'); // import the password middleware
 
 const userCtrl = require('../controllers/user'); // import the user controller
 
 
-router.post('/signup', userCtrl.signup); // create a new user
+router.post('/signup', password, userCtrl.signup); // create a new user
 router.post('/login',max.apiLimit, userCtrl.login); // login a user
 
 
