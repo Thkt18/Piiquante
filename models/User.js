4c1848892806554ef3d17c97110cfd2f0e5dd3cc
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2023-02-04 11:35:12 +0100

    Ajout de "validator"

--- models/User.js
+++ models/User.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose');
 // Mongoose-unique-validator permet de vérifier que les données sont uniques
 const uniqueValidator = require('mongoose-unique-validator');
 require('mongoose-type-email'); // Mongoose-type-email permet de vérifier que l'email est valide
+const validatorPackage = require('validator'); // Validator permet de vérifier que l'email est valide
 
 // Création du schéma de données pour les utilisateurs
 const userSchema = mongoose.Schema({
-  email: { type: mongoose.SchemaTypes.Email, required: true, unique: true }, // Email unique grâce à unique-validator
+  email: 
+  { type: mongoose.SchemaTypes.Email,
+    
+    validate:{
+    validator: validatorPackage.isEmail,
+    message: 'Email incorrect !'
+  }, 
+  required: true,
+  unique: true }, // Email unique grâce à unique-validator
   password: { type: String, required: true }
 });
 
